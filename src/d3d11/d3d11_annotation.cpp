doitsujin/dxvk

[d3d11] Register annotation interfaces with D3D9
Some apps try use the D3DPERF_ functions for debug markers/annotations.

This utilizes the DXVK_RegisterAnnotation hidden functions to share the interfaces.

src/d3d11/d3d11_annotation.cpp
@@ -6,13 +6,41 @@
 
 namespace dxvk {
 
+  template <bool Register>
+  static void RegisterUserDefinedAnnotation(IDXVKUserDefinedAnnotation* annotation) {
+    using RegistrationFunctionType = void(__stdcall *)(IDXVKUserDefinedAnnotation*);
+    static const int16_t RegisterOrdinal = 28257;
+    static const int16_t UnregisterOrdinal = 28258;
+
+    HMODULE d3d9Module = ::LoadLibraryA("d3d9.dll");
+    if (!d3d9Module) {
+      Logger::info("Unable to find d3d9, some annotations may be missed.");
+      return;
+    }
+
+    const int16_t ordinal = Register ? UnregisterOrdinal : RegisterOrdinal;
+    auto registrationFunction = reinterpret_cast<RegistrationFunctionType>(::GetProcAddress(d3d9Module,
+      reinterpret_cast<const char*>(static_cast<uintptr_t>(ordinal))));
+
+    if (!registrationFunction) {
+      Logger::info("Unable to find DXVK_RegisterAnnotation, some annotations may be missed.");
+      return;
+    }
+
+    registrationFunction(annotation);
+  }
+
   D3D11UserDefinedAnnotation::D3D11UserDefinedAnnotation(D3D11DeviceContext* ctx)
   : m_container(ctx),
-    m_eventDepth(0) { }
+    m_eventDepth(0) {
+    if (m_container->IsAnnotationEnabled())
+      RegisterUserDefinedAnnotation<true>(this);
+  }
 
 
   D3D11UserDefinedAnnotation::~D3D11UserDefinedAnnotation() {
-
+    if (m_container->IsAnnotationEnabled())
+      RegisterUserDefinedAnnotation<false>(this);
   }
 
 

k8se10/MW32011NCP

Co-authored-by: Oleg Kuznetsov <okouznetsov@nvidia.com>

src/d3d11/d3d11_annotation.cpp
@@ -6,13 +6,46 @@
 
 namespace dxvk {
 
+  template <bool Register>
+  static void RegisterUserDefinedAnnotation(IDXVKUserDefinedAnnotation* annotation) {
+    using RegistrationFunctionType = void(__stdcall *)(IDXVKUserDefinedAnnotation*);
+    static const int16_t RegisterOrdinal = 28257;
+    static const int16_t UnregisterOrdinal = 28258;
+
+    HMODULE d3d9Module = ::LoadLibraryA("d3d9.dll");
+    if (!d3d9Module) {
+      Logger::info("Unable to find d3d9, some annotations may be missed.");
+      return;
+    }
+
+    const int16_t ordinal = Register ? RegisterOrdinal : UnregisterOrdinal;
+    auto registrationFunction = reinterpret_cast<RegistrationFunctionType>(::GetProcAddress(d3d9Module,
+      reinterpret_cast<const char*>(static_cast<uintptr_t>(ordinal))));
+
+    if (!registrationFunction) {
+      Logger::info("Unable to find DXVK_RegisterAnnotation, some annotations may be missed.");
+      return;
+    }
+
+    registrationFunction(annotation);
+  }
+
   D3D11UserDefinedAnnotation::D3D11UserDefinedAnnotation(D3D11DeviceContext* ctx)
   : m_container(ctx),
-    m_eventDepth(0) { }
+    m_eventDepth(0) {
+    if (m_container->IsAnnotationEnabled())
+      RegisterUserDefinedAnnotation<true>(this);
+  }
 
+  D3D11UserDefinedAnnotation::D3D11UserDefinedAnnotation(const D3D11UserDefinedAnnotation&)
+  {
+    if (m_container->IsAnnotationEnabled())
+      RegisterUserDefinedAnnotation<true>(this);
+  }
 
   D3D11UserDefinedAnnotation::~D3D11UserDefinedAnnotation() {
-
+    if (m_container->IsAnnotationEnabled())
+      RegisterUserDefinedAnnotation<false>(this);
   }
 
 
@@ -38,6 +71,8 @@ namespace dxvk {
           LPCWSTR                 Name) {
     if (!m_container->IsAnnotationEnabled())
       return -1;
+
+    D3D10DeviceLock lock = m_container->LockContext();
 
     m_container->EmitCs([color = Color, labelName = dxvk::str::fromws(Name)](DxvkContext *ctx) {
       VkDebugUtilsLabelEXT label;
@@ -72,6 +107,8 @@ namespace dxvk {
           LPCWSTR                 Name) {
     if (!m_container->IsAnnotationEnabled())
       return;
+
+    D3D10DeviceLock lock = m_container->LockContext();
 
     m_container->EmitCs([color = Color, labelName = dxvk::str::fromws(Name)](DxvkContext *ctx) {
       VkDebugUtilsLabelEXT label;