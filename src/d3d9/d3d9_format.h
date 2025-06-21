WinterSnowfall/dxvk

[d3d8/9] Clean up CheckDeviceFormat and CreateImageSurface formats

src/d3d9/d3d9_format.h
@@ -221,8 +221,10 @@ namespace dxvk {
 
     bool CheckImageFormatSupport(
       const Rc<DxvkAdapter>&      Adapter,
-      VkFormat              Format,
-      VkFormatFeatureFlags2 Features) const;
+            VkFormat              Format,
+            VkFormatFeatureFlags2 Features) const;
+
+    D3D9Adapter* m_adapter = nullptr;
 
     bool m_d24s8Support;
     bool m_d16s8Support;

k8se10/MW32011NCP

src/d3d9/d3d9_format.h
@@ -221,8 +221,10 @@ namespace dxvk {
 
     bool CheckImageFormatSupport(
       const Rc<DxvkAdapter>&      Adapter,
-      VkFormat              Format,
-      VkFormatFeatureFlags2 Features) const;
+            VkFormat              Format,
+            VkFormatFeatureFlags2 Features) const;
+
+    D3D9Adapter* m_parent = nullptr;
 
     bool m_d24s8Support;
     bool m_d16s8Support;