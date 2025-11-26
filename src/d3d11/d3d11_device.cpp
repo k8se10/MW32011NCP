doitsujin/dxvk

[d3d11] Use the D3DKMT API to open shared resources
Reading the shared resource runtime private data should work with native
D3D resource handles, although the runtime private data format is still
different.

src/d3d11/d3d11_device.cpp
@@ -38,7 +38,6 @@ namespace dxvk {
   constexpr uint32_t D3D11DXGIDevice::DefaultFrameLatency;
 
 
-
   D3D11Device::D3D11Device(
           D3D11DXGIDevice*    pContainer,
           D3D_FEATURE_LEVEL   FeatureLevel,
@@ -1443,6 +1442,63 @@ namespace dxvk {
           HANDLE      hResource,
           REFIID      ReturnedInterface,
           void**      ppResource) {
+    InitReturnPtr(ppResource);
+
+    if (!((uintptr_t)hResource & 0xc0000000)) {
+      Logger::warn("D3D11Device::OpenSharedResource: Invalid shared handle type");
+      return E_INVALIDARG;
+    }
+
+    if (ppResource == nullptr)
+      return S_FALSE;
+
+    union d3dkmt_desc d3dkmt;
+
+    D3DKMT_QUERYRESOURCEINFO query = { };
+    query.hDevice = m_dxvkDevice->kmtLocal();
+    query.hGlobalShare = (uintptr_t)hResource;
+    query.pPrivateRuntimeData = &d3dkmt;
+    query.PrivateRuntimeDataSize = sizeof(d3dkmt);
+
+    if (D3DKMTQueryResourceInfo(&query)) {
+      Logger::warn(str::format("D3D11Device::OpenSharedResource: Failed to query resource: ", hResource));
+    } else if (query.PrivateRuntimeDataSize < sizeof(d3dkmt.dxgi) || query.PrivateRuntimeDataSize > sizeof(d3dkmt)) {
+      Logger::warn(str::format("D3D11Device::OpenSharedResource: Unexpected size: ", query.PrivateRuntimeDataSize));
+    } else {
+      D3DDDI_OPENALLOCATIONINFO2 alloc = { };
+      D3DKMT_OPENRESOURCE open = { };
+      open.hDevice = m_dxvkDevice->kmtLocal();
+      open.hGlobalShare = (uintptr_t)hResource;
+      open.NumAllocations = 1;
+      open.pOpenAllocationInfo2 = &alloc;
+      open.pPrivateRuntimeData = &d3dkmt;
+      open.PrivateRuntimeDataSize = query.PrivateRuntimeDataSize;
+
+      if (D3DKMTOpenResource2(&open)) {
+        Logger::warn(str::format("D3D11Device::OpenSharedResource: Failed to open resource: ", hResource));
+      } else {
+        D3DKMT_DESTROYALLOCATION destroy = { };
+        destroy.hDevice = m_dxvkDevice->kmtLocal();
+        destroy.hResource = open.hResource;
+        D3DKMTDestroyAllocation(&destroy);
+
+        D3D11_COMMON_TEXTURE_DESC desc = { };
+        if (!ConvertRuntimeDescriptor(query.PrivateRuntimeDataSize, d3dkmt, &desc))
+          return E_INVALIDARG;
+
+        try {
+          const Com<D3D11Texture2D> texture = new D3D11Texture2D(this, &desc, nullptr, hResource);
+          texture->QueryInterface(ReturnedInterface, ppResource);
+          return S_OK;
+        }
+        catch (const DxvkError& e) {
+          Logger::err(e.message());
+          return E_INVALIDARG;
+        }
+      }
+    }
+
+    /* try the legacy Proton shared resource implementation */
     return OpenSharedResourceGeneric<true>(
       hResource, ReturnedInterface, ppResource);
   }
@@ -1452,6 +1508,80 @@ namespace dxvk {
           HANDLE      hResource,
           REFIID      ReturnedInterface,
           void**      ppResource) {
+    InitReturnPtr(ppResource);
+
+    if ((uintptr_t)hResource & 0xc0000000) {
+      Logger::warn("D3D11Device::OpenSharedResource1: Invalid shared handle type");
+      return E_INVALIDARG;
+    }
+
+    if (ppResource == nullptr)
+      return S_FALSE;
+
+    union d3dkmt_desc d3dkmt;
+
+    D3DKMT_QUERYRESOURCEINFOFROMNTHANDLE query = { };
+    query.hDevice = m_dxvkDevice->kmtLocal();
+    query.hNtHandle = hResource;
+    query.pPrivateRuntimeData = &d3dkmt;
+    query.PrivateRuntimeDataSize = sizeof(d3dkmt);
+
+    if (D3DKMTQueryResourceInfoFromNtHandle(&query)) {
+      Logger::warn(str::format("D3D11Device::OpenSharedResource1: Failed to query resource: ", hResource));
+    } else if (query.PrivateRuntimeDataSize < sizeof(d3dkmt.dxgi) || query.PrivateRuntimeDataSize > sizeof(d3dkmt)) {
+      Logger::warn(str::format("D3D11Device::OpenSharedResource1: Unexpected size: ", query.PrivateRuntimeDataSize));
+    } else {
+      D3DDDI_OPENALLOCATIONINFO2 alloc = { };
+      D3DKMT_OPENRESOURCEFROMNTHANDLE open = { };
+      char dummy;
+
+      open.hDevice = m_dxvkDevice->kmtLocal();
+      open.hNtHandle = hResource;
+      open.NumAllocations = 1;
+      open.pOpenAllocationInfo2 = &alloc;
+      open.pPrivateRuntimeData = &d3dkmt;
+      open.PrivateRuntimeDataSize = query.PrivateRuntimeDataSize;
+      open.pTotalPrivateDriverDataBuffer = &dummy;
+      open.TotalPrivateDriverDataBufferSize = 0;
+
+      if (D3DKMTOpenResourceFromNtHandle(&open)) {
+        Logger::warn(str::format("D3D11Device::OpenSharedResource1: Failed to open resource: ", hResource));
+      } else {
+        D3DKMT_DESTROYALLOCATION destroy = { };
+        destroy.hDevice = m_dxvkDevice->kmtLocal();
+        destroy.hResource = open.hResource;
+        D3DKMTDestroyAllocation(&destroy);
+
+        if (open.hKeyedMutex) {
+          Logger::warn(str::format("D3D11Device::OpenSharedResource1: Ignoring bundled keyed mutex"));
+          D3DKMT_DESTROYKEYEDMUTEX destroy_mutex = { };
+          destroy_mutex.hKeyedMutex = open.hKeyedMutex;
+          D3DKMTDestroyKeyedMutex(&destroy_mutex);
+        }
+        if (open.hSyncObject) {
+          Logger::warn(str::format("D3D11Device::OpenSharedResource1: Ignoring bundled sync object"));
+          D3DKMT_DESTROYSYNCHRONIZATIONOBJECT destroy_sync = { };
+          destroy_sync.hSyncObject = open.hSyncObject;
+          D3DKMTDestroySynchronizationObject(&destroy_sync);
+        }
+
+        D3D11_COMMON_TEXTURE_DESC desc = { };
+        if (!ConvertRuntimeDescriptor(query.PrivateRuntimeDataSize, d3dkmt, &desc))
+          return E_INVALIDARG;
+
+        try {
+          const Com<D3D11Texture2D> texture = new D3D11Texture2D(this, &desc, nullptr, hResource);
+          texture->QueryInterface(ReturnedInterface, ppResource);
+          return S_OK;
+        }
+        catch (const DxvkError& e) {
+          Logger::err(e.message());
+          return E_INVALIDARG;
+        }
+      }
+    }
+
+    /* try the legacy Proton shared resource implementation */
     return OpenSharedResourceGeneric<false>(
       hResource, ReturnedInterface, ppResource);
   }
@@ -2406,11 +2536,6 @@ namespace dxvk {
           HANDLE      hResource,
           REFIID      ReturnedInterface,
           void**      ppResource) {
-    InitReturnPtr(ppResource);
-
-    if (ppResource == nullptr)
-      return S_FALSE;
-
 #ifdef _WIN32
     HANDLE ntHandle = IsKmtHandle ? openKmtHandle(hResource) : hResource;
 
@@ -2598,6 +2723,127 @@ namespace dxvk {
       result.flags.set(DxvkShaderCompileFlag::EnableSampleRateShading);
 
     return result;
+  }
+
+
+  bool D3D11Device::ConvertRuntimeDescriptor(
+       UINT                       size,
+       const union d3dkmt_desc&   d3dkmt,
+       D3D11_COMMON_TEXTURE_DESC* desc) {
+
+    if (size == sizeof(d3dkmt.d3d12) && d3dkmt.d3d12.d3d11.dxgi.size == sizeof(d3dkmt.d3d12.d3d11) && d3dkmt.d3d12.d3d11.dxgi.version == 0) {
+      Logger::warn(str::format("D3D11Device::ConvertRuntimeDescriptor: D3D12 descriptor conversion not implemented"));
+      return false;
+    }
+
+    if (size >= sizeof(d3dkmt.d3d11) && d3dkmt.dxgi.size == sizeof(d3dkmt.d3d11) && d3dkmt.dxgi.version == 4) {
+      Logger::info(str::format("D3D11Device::ConvertRuntimeDescriptor: Found D3D11 desc with dimension: ", d3dkmt.d3d11.dimension));
+
+      switch (d3dkmt.d3d11.dimension) {
+        case D3D11_RESOURCE_DIMENSION_TEXTURE2D:
+          desc->Width = d3dkmt.d3d11.d3d11_2d.Width;
+          desc->Height = d3dkmt.d3d11.d3d11_2d.Height;
+          desc->Depth = 1;
+          desc->MipLevels = d3dkmt.d3d11.d3d11_2d.MipLevels;
+          desc->ArraySize = d3dkmt.d3d11.d3d11_2d.ArraySize;
+          desc->Format = d3dkmt.d3d11.d3d11_2d.Format;
+          desc->SampleDesc = d3dkmt.d3d11.d3d11_2d.SampleDesc;
+          desc->Usage = d3dkmt.d3d11.d3d11_2d.Usage;
+          desc->BindFlags = d3dkmt.d3d11.d3d11_2d.BindFlags;
+          desc->CPUAccessFlags = d3dkmt.d3d11.d3d11_2d.CPUAccessFlags;
+          desc->MiscFlags = d3dkmt.d3d11.d3d11_2d.MiscFlags;
+          desc->TextureLayout = D3D11_TEXTURE_LAYOUT_UNDEFINED;
+          break;
+        default:
+          Logger::warn(str::format("D3D11Device::ConvertRuntimeDescriptor: Unsupported dimension: ", d3dkmt.d3d11.dimension));
+          return false;
+      }
+
+      Logger::info(str::format("D3D11Device::ConvertRuntimeDescriptor: Translated D3D11 desc:"));
+      Logger::info(str::format("  Width: ", desc->Width));
+      Logger::info(str::format("  Height: ", desc->Height));
+      Logger::info(str::format("  Depth: ", desc->Depth));
+      Logger::info(str::format("  MipLevels: ", desc->MipLevels));
+      Logger::info(str::format("  ArraySize: ", desc->ArraySize));
+      Logger::info(str::format("  Format: ", desc->Format));
+      Logger::info(str::format("  SampleDesc.Count: ", desc->SampleDesc.Count));
+      Logger::info(str::format("  SampleDesc.Quality: ", desc->SampleDesc.Quality));
+      Logger::info(str::format("  Usage: ", desc->Usage));
+      Logger::info(str::format("  BindFlags: ", desc->BindFlags));
+      Logger::info(str::format("  CPUAccessFlags: ", desc->CPUAccessFlags));
+      Logger::info(str::format("  MiscFlags: ", desc->MiscFlags));
+      Logger::info(str::format("  TextureLayout: ", desc->TextureLayout));
+      return true;
+    }
+
+    if (size >= sizeof(d3dkmt.d3d9) && d3dkmt.dxgi.size == sizeof(d3dkmt.d3d9) && d3dkmt.dxgi.version == 1) {
+      Logger::info(str::format("D3D11Device::ConvertRuntimeDescriptor: Found D3D9 desc: ", d3dkmt.d3d9.type));
+      Logger::info(str::format("  dxgi.width: ", d3dkmt.d3d9.dxgi.width));
+      Logger::info(str::format("  dxgi.height: ", d3dkmt.d3d9.dxgi.height));
+      Logger::info(str::format("  format: ", d3dkmt.d3d9.format));
+      Logger::info(str::format("  usage: ", d3dkmt.d3d9.usage));
+      if (d3dkmt.d3d9.type == D3DRTYPE_TEXTURE) {
+        Logger::info(str::format("  texture.width: ", d3dkmt.d3d9.texture.width));
+        Logger::info(str::format("  texture.height: ", d3dkmt.d3d9.texture.height));
+        Logger::info(str::format("  texture.depth: ", d3dkmt.d3d9.texture.depth));
+        Logger::info(str::format("  texture.levels: ", d3dkmt.d3d9.texture.levels));
+      } else if (d3dkmt.d3d9.type == D3DRTYPE_SURFACE) {
+        Logger::info(str::format("  surface.width: ", d3dkmt.d3d9.surface.width));
+        Logger::info(str::format("  surface.height: ", d3dkmt.d3d9.surface.height));
+      } else {
+        Logger::warn(str::format("D3D11Device::ConvertRuntimeDescriptor: Unsupported D3D9 type: ", d3dkmt.d3d9.type));
+        return false;
+      }
+
+      desc->Width = d3dkmt.d3d9.dxgi.width;
+      desc->Height = d3dkmt.d3d9.dxgi.height;
+      desc->Depth = 1;
+      desc->MipLevels = 1;
+      desc->ArraySize = 1;
+      desc->Format = d3dkmt.d3d9.dxgi.format;
+      desc->SampleDesc.Count = 1;
+      desc->SampleDesc.Quality = 0;
+      desc->Usage = D3D11_USAGE_DEFAULT;
+      desc->BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+      desc->CPUAccessFlags = 0;
+      desc->MiscFlags = D3D11_RESOURCE_MISC_SHARED;
+      desc->TextureLayout = D3D11_TEXTURE_LAYOUT_UNDEFINED;
+
+      switch (d3dkmt.d3d9.type) {
+        case D3DRTYPE_TEXTURE:
+          desc->Width = d3dkmt.d3d9.texture.width;
+          desc->Height = d3dkmt.d3d9.texture.height;
+          desc->MipLevels = d3dkmt.d3d9.texture.levels;
+          desc->ArraySize = d3dkmt.d3d9.texture.depth ? d3dkmt.d3d9.texture.depth : 1;
+          break;
+        case D3DRTYPE_SURFACE:
+          desc->Width = d3dkmt.d3d9.surface.width;
+          desc->Height = d3dkmt.d3d9.surface.height;
+          break;
+        default:
+          break;
+      }
+
+      Logger::info(str::format("D3D11Device::ConvertRuntimeDescriptor: Translated D3D9 desc:"));
+      Logger::info(str::format("  Width: ", desc->Width));
+      Logger::info(str::format("  Height: ", desc->Height));
+      Logger::info(str::format("  Depth: ", desc->Depth));
+      Logger::info(str::format("  MipLevels: ", desc->MipLevels));
+      Logger::info(str::format("  ArraySize: ", desc->ArraySize));
+      Logger::info(str::format("  Format: ", desc->Format));
+      Logger::info(str::format("  SampleDesc.Count: ", desc->SampleDesc.Count));
+      Logger::info(str::format("  SampleDesc.Quality: ", desc->SampleDesc.Quality));
+      Logger::info(str::format("  Usage: ", desc->Usage));
+      Logger::info(str::format("  BindFlags: ", desc->BindFlags));
+      Logger::info(str::format("  CPUAccessFlags: ", desc->CPUAccessFlags));
+      Logger::info(str::format("  MiscFlags: ", desc->MiscFlags));
+      Logger::info(str::format("  TextureLayout: ", desc->TextureLayout));
+      return true;
+    }
+
+    Logger::warn(str::format("D3D11Device::ConvertRuntimeDescriptor: Unsupported runtime desc size: ",
+                             size, "/", d3dkmt.dxgi.size, " version: ", d3dkmt.dxgi.version));
+    return false;
   }
 
 

k8se10/MW32011NCP

src/d3d11/d3d11_device.cpp
@@ -1443,6 +1443,63 @@ namespace dxvk {
           HANDLE      hResource,
           REFIID      ReturnedInterface,
           void**      ppResource) {
+    InitReturnPtr(ppResource);
+
+    if (!(reinterpret_cast<uintptr_t>(hResource) & 0xc0000000)) {
+      Logger::warn("D3D11Device::OpenSharedResource: Invalid shared handle type");
+      return E_INVALIDARG;
+    }
+
+    if (ppResource == nullptr)
+      return S_FALSE;
+
+    union d3dkmt_desc d3dkmt;
+
+    D3DKMT_QUERYRESOURCEINFO query = { };
+    query.hDevice = m_dxvkDevice->kmtLocal();
+    query.hGlobalShare = reinterpret_cast<uintptr_t>(hResource);
+    query.pPrivateRuntimeData = &d3dkmt;
+    query.PrivateRuntimeDataSize = sizeof(d3dkmt);
+
+    if (D3DKMTQueryResourceInfo(&query)) {
+      Logger::warn(str::format("D3D11Device::OpenSharedResource: Failed to query resource: ", hResource));
+    } else if (query.PrivateRuntimeDataSize < sizeof(d3dkmt.dxgi) || query.PrivateRuntimeDataSize > sizeof(d3dkmt)) {
+      Logger::warn(str::format("D3D11Device::OpenSharedResource: Unexpected size: ", query.PrivateRuntimeDataSize));
+    } else {
+      D3DDDI_OPENALLOCATIONINFO2 alloc = { };
+      D3DKMT_OPENRESOURCE open = { };
+      open.hDevice = m_dxvkDevice->kmtLocal();
+      open.hGlobalShare = reinterpret_cast<uintptr_t>(hResource);
+      open.NumAllocations = 1;
+      open.pOpenAllocationInfo2 = &alloc;
+      open.pPrivateRuntimeData = &d3dkmt;
+      open.PrivateRuntimeDataSize = query.PrivateRuntimeDataSize;
+
+      if (D3DKMTOpenResource2(&open)) {
+        Logger::warn(str::format("D3D11Device::OpenSharedResource: Failed to open resource: ", hResource));
+      } else {
+        D3DKMT_DESTROYALLOCATION destroy = { };
+        destroy.hDevice = m_dxvkDevice->kmtLocal();
+        destroy.hResource = open.hResource;
+        D3DKMTDestroyAllocation(&destroy);
+
+        D3D11_COMMON_TEXTURE_DESC desc = { };
+        if (!ConvertRuntimeDescriptor(query.PrivateRuntimeDataSize, d3dkmt, &desc))
+          return E_INVALIDARG;
+
+        try {
+          const Com<D3D11Texture2D> texture = new D3D11Texture2D(this, &desc, nullptr, hResource);
+          texture->QueryInterface(ReturnedInterface, ppResource);
+          return S_OK;
+        }
+        catch (const DxvkError& e) {
+          Logger::err(e.message());
+          return E_INVALIDARG;
+        }
+      }
+    }
+
+    /* try the legacy Proton shared resource implementation */
     return OpenSharedResourceGeneric<true>(
       hResource, ReturnedInterface, ppResource);
   }
@@ -1452,6 +1509,80 @@ namespace dxvk {
           HANDLE      hResource,
           REFIID      ReturnedInterface,
           void**      ppResource) {
+    InitReturnPtr(ppResource);
+
+    if (reinterpret_cast<uintptr_t>(hResource) & 0xc0000000) {
+      Logger::warn("D3D11Device::OpenSharedResource1: Invalid shared handle type");
+      return E_INVALIDARG;
+    }
+
+    if (ppResource == nullptr)
+      return S_FALSE;
+
+    union d3dkmt_desc d3dkmt;
+
+    D3DKMT_QUERYRESOURCEINFOFROMNTHANDLE query = { };
+    query.hDevice = m_dxvkDevice->kmtLocal();
+    query.hNtHandle = hResource;
+    query.pPrivateRuntimeData = &d3dkmt;
+    query.PrivateRuntimeDataSize = sizeof(d3dkmt);
+
+    if (D3DKMTQueryResourceInfoFromNtHandle(&query)) {
+      Logger::warn(str::format("D3D11Device::OpenSharedResource1: Failed to query resource: ", hResource));
+    } else if (query.PrivateRuntimeDataSize < sizeof(d3dkmt.dxgi) || query.PrivateRuntimeDataSize > sizeof(d3dkmt)) {
+      Logger::warn(str::format("D3D11Device::OpenSharedResource1: Unexpected size: ", query.PrivateRuntimeDataSize));
+    } else {
+      D3DDDI_OPENALLOCATIONINFO2 alloc = { };
+      D3DKMT_OPENRESOURCEFROMNTHANDLE open = { };
+      char dummy;
+
+      open.hDevice = m_dxvkDevice->kmtLocal();
+      open.hNtHandle = hResource;
+      open.NumAllocations = 1;
+      open.pOpenAllocationInfo2 = &alloc;
+      open.pPrivateRuntimeData = &d3dkmt;
+      open.PrivateRuntimeDataSize = query.PrivateRuntimeDataSize;
+      open.pTotalPrivateDriverDataBuffer = &dummy;
+      open.TotalPrivateDriverDataBufferSize = 0;
+
+      if (D3DKMTOpenResourceFromNtHandle(&open)) {
+        Logger::warn(str::format("D3D11Device::OpenSharedResource1: Failed to open resource: ", hResource));
+      } else {
+        D3DKMT_DESTROYALLOCATION destroy = { };
+        destroy.hDevice = m_dxvkDevice->kmtLocal();
+        destroy.hResource = open.hResource;
+        D3DKMTDestroyAllocation(&destroy);
+
+        if (open.hSyncObject) {
+          Logger::warn(str::format("D3D11Device::OpenSharedResource1: Ignoring bundled sync object"));
+          D3DKMT_DESTROYSYNCHRONIZATIONOBJECT destroySync = { };
+          destroySync.hSyncObject = open.hSyncObject;
+          D3DKMTDestroySynchronizationObject(&destroySync);
+        }
+        if (open.hKeyedMutex) {
+          Logger::warn(str::format("D3D11Device::OpenSharedResource1: Ignoring bundled keyed mutex"));
+          D3DKMT_DESTROYKEYEDMUTEX destroyMutex = { };
+          destroyMutex.hKeyedMutex = open.hKeyedMutex;
+          D3DKMTDestroyKeyedMutex(&destroyMutex);
+        }
+
+        D3D11_COMMON_TEXTURE_DESC desc = { };
+        if (!ConvertRuntimeDescriptor(query.PrivateRuntimeDataSize, d3dkmt, &desc))
+          return E_INVALIDARG;
+
+        try {
+          const Com<D3D11Texture2D> texture = new D3D11Texture2D(this, &desc, nullptr, hResource);
+          texture->QueryInterface(ReturnedInterface, ppResource);
+          return S_OK;
+        }
+        catch (const DxvkError& e) {
+          Logger::err(e.message());
+          return E_INVALIDARG;
+        }
+      }
+    }
+
+    /* try the legacy Proton shared resource implementation */
     return OpenSharedResourceGeneric<false>(
       hResource, ReturnedInterface, ppResource);
   }
@@ -2406,11 +2537,6 @@ namespace dxvk {
           HANDLE      hResource,
           REFIID      ReturnedInterface,
           void**      ppResource) {
-    InitReturnPtr(ppResource);
-
-    if (ppResource == nullptr)
-      return S_FALSE;
-
 #ifdef _WIN32
     HANDLE ntHandle = IsKmtHandle ? openKmtHandle(hResource) : hResource;
 
@@ -2598,6 +2724,127 @@ namespace dxvk {
       result.flags.set(DxvkShaderCompileFlag::EnableSampleRateShading);
 
     return result;
+  }
+
+
+  bool D3D11Device::ConvertRuntimeDescriptor(
+       UINT                       size,
+       const union d3dkmt_desc&   d3dkmt,
+       D3D11_COMMON_TEXTURE_DESC* desc) {
+
+    if (size == sizeof(d3dkmt.d3d12) && d3dkmt.d3d12.d3d11.dxgi.size == sizeof(d3dkmt.d3d12.d3d11) && d3dkmt.d3d12.d3d11.dxgi.version == 0) {
+      Logger::warn(str::format("D3D11Device::ConvertRuntimeDescriptor: D3D12 descriptor conversion not implemented"));
+      return false;
+    }
+
+    if (size >= sizeof(d3dkmt.d3d11) && d3dkmt.dxgi.size == sizeof(d3dkmt.d3d11) && d3dkmt.dxgi.version == 4) {
+      Logger::debug(str::format("D3D11Device::ConvertRuntimeDescriptor: Found D3D11 desc with dimension: ", d3dkmt.d3d11.dimension));
+
+      switch (d3dkmt.d3d11.dimension) {
+        case D3D11_RESOURCE_DIMENSION_TEXTURE2D:
+          desc->Width = d3dkmt.d3d11.d3d11_2d.Width;
+          desc->Height = d3dkmt.d3d11.d3d11_2d.Height;
+          desc->Depth = 1;
+          desc->MipLevels = d3dkmt.d3d11.d3d11_2d.MipLevels;
+          desc->ArraySize = d3dkmt.d3d11.d3d11_2d.ArraySize;
+          desc->Format = d3dkmt.d3d11.d3d11_2d.Format;
+          desc->SampleDesc = d3dkmt.d3d11.d3d11_2d.SampleDesc;
+          desc->Usage = d3dkmt.d3d11.d3d11_2d.Usage;
+          desc->BindFlags = d3dkmt.d3d11.d3d11_2d.BindFlags;
+          desc->CPUAccessFlags = d3dkmt.d3d11.d3d11_2d.CPUAccessFlags;
+          desc->MiscFlags = d3dkmt.d3d11.d3d11_2d.MiscFlags;
+          desc->TextureLayout = D3D11_TEXTURE_LAYOUT_UNDEFINED;
+          break;
+        default:
+          Logger::warn(str::format("D3D11Device::ConvertRuntimeDescriptor: Unsupported dimension: ", d3dkmt.d3d11.dimension));
+          return false;
+      }
+
+      Logger::debug(str::format("D3D11Device::ConvertRuntimeDescriptor: Translated D3D11 desc:"));
+      Logger::debug(str::format("  Width: ", desc->Width));
+      Logger::debug(str::format("  Height: ", desc->Height));
+      Logger::debug(str::format("  Depth: ", desc->Depth));
+      Logger::debug(str::format("  MipLevels: ", desc->MipLevels));
+      Logger::debug(str::format("  ArraySize: ", desc->ArraySize));
+      Logger::debug(str::format("  Format: ", desc->Format));
+      Logger::debug(str::format("  SampleDesc.Count: ", desc->SampleDesc.Count));
+      Logger::debug(str::format("  SampleDesc.Quality: ", desc->SampleDesc.Quality));
+      Logger::debug(str::format("  Usage: ", desc->Usage));
+      Logger::debug(str::format("  BindFlags: ", desc->BindFlags));
+      Logger::debug(str::format("  CPUAccessFlags: ", desc->CPUAccessFlags));
+      Logger::debug(str::format("  MiscFlags: ", desc->MiscFlags));
+      Logger::debug(str::format("  TextureLayout: ", desc->TextureLayout));
+      return true;
+    }
+
+    if (size >= sizeof(d3dkmt.d3d9) && d3dkmt.dxgi.size == sizeof(d3dkmt.d3d9) && d3dkmt.dxgi.version == 1) {
+      Logger::debug(str::format("D3D11Device::ConvertRuntimeDescriptor: Found D3D9 desc: ", d3dkmt.d3d9.type));
+      Logger::debug(str::format("  dxgi.width: ", d3dkmt.d3d9.dxgi.width));
+      Logger::debug(str::format("  dxgi.height: ", d3dkmt.d3d9.dxgi.height));
+      Logger::debug(str::format("  format: ", d3dkmt.d3d9.format));
+      Logger::debug(str::format("  usage: ", d3dkmt.d3d9.usage));
+      if (d3dkmt.d3d9.type == D3DRTYPE_TEXTURE) {
+        Logger::debug(str::format("  texture.width: ", d3dkmt.d3d9.texture.width));
+        Logger::debug(str::format("  texture.height: ", d3dkmt.d3d9.texture.height));
+        Logger::debug(str::format("  texture.depth: ", d3dkmt.d3d9.texture.depth));
+        Logger::debug(str::format("  texture.levels: ", d3dkmt.d3d9.texture.levels));
+      } else if (d3dkmt.d3d9.type == D3DRTYPE_SURFACE) {
+        Logger::debug(str::format("  surface.width: ", d3dkmt.d3d9.surface.width));
+        Logger::debug(str::format("  surface.height: ", d3dkmt.d3d9.surface.height));
+      } else {
+        Logger::warn(str::format("D3D11Device::ConvertRuntimeDescriptor: Unsupported D3D9 type: ", d3dkmt.d3d9.type));
+        return false;
+      }
+
+      desc->Width = d3dkmt.d3d9.dxgi.width;
+      desc->Height = d3dkmt.d3d9.dxgi.height;
+      desc->Depth = 1;
+      desc->MipLevels = 1;
+      desc->ArraySize = 1;
+      desc->Format = d3dkmt.d3d9.dxgi.format;
+      desc->SampleDesc.Count = 1;
+      desc->SampleDesc.Quality = 0;
+      desc->Usage = D3D11_USAGE_DEFAULT;
+      desc->BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+      desc->CPUAccessFlags = 0;
+      desc->MiscFlags = D3D11_RESOURCE_MISC_SHARED;
+      desc->TextureLayout = D3D11_TEXTURE_LAYOUT_UNDEFINED;
+
+      switch (d3dkmt.d3d9.type) {
+        case D3DRTYPE_TEXTURE:
+          desc->Width = d3dkmt.d3d9.texture.width;
+          desc->Height = d3dkmt.d3d9.texture.height;
+          desc->MipLevels = d3dkmt.d3d9.texture.levels;
+          desc->ArraySize = d3dkmt.d3d9.texture.depth ? d3dkmt.d3d9.texture.depth : 1;
+          break;
+        case D3DRTYPE_SURFACE:
+          desc->Width = d3dkmt.d3d9.surface.width;
+          desc->Height = d3dkmt.d3d9.surface.height;
+          break;
+        default:
+          break;
+      }
+
+      Logger::debug(str::format("D3D11Device::ConvertRuntimeDescriptor: Translated D3D9 desc:"));
+      Logger::debug(str::format("  Width: ", desc->Width));
+      Logger::debug(str::format("  Height: ", desc->Height));
+      Logger::debug(str::format("  Depth: ", desc->Depth));
+      Logger::debug(str::format("  MipLevels: ", desc->MipLevels));
+      Logger::debug(str::format("  ArraySize: ", desc->ArraySize));
+      Logger::debug(str::format("  Format: ", desc->Format));
+      Logger::debug(str::format("  SampleDesc.Count: ", desc->SampleDesc.Count));
+      Logger::debug(str::format("  SampleDesc.Quality: ", desc->SampleDesc.Quality));
+      Logger::debug(str::format("  Usage: ", desc->Usage));
+      Logger::debug(str::format("  BindFlags: ", desc->BindFlags));
+      Logger::debug(str::format("  CPUAccessFlags: ", desc->CPUAccessFlags));
+      Logger::debug(str::format("  MiscFlags: ", desc->MiscFlags));
+      Logger::debug(str::format("  TextureLayout: ", desc->TextureLayout));
+      return true;
+    }
+
+    Logger::warn(str::format("D3D11Device::ConvertRuntimeDescriptor: Unsupported runtime desc size: ",
+                             size, "/", d3dkmt.dxgi.size, " version: ", d3dkmt.dxgi.version));
+    return false;
   }
 
 