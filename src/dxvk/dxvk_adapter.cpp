k8se10/MW32011NCP

commit 8e5b70522118139ddc71d7c4193f530413045c36

[dxvk] Expose queue ownership transfer properties in device

src/dxvk/dxvk_adapter.cpp
@@ -7,10 +7,11 @@
 
 namespace dxvk {
 
-  DxvkDeviceQueue getDeviceQueue(const Rc<vk::DeviceFn>& vkd, DxvkDeviceQueueIndex queue) {
+  DxvkDeviceQueue getDeviceQueue(const Rc<vk::DeviceFn>& vkd, const DxvkDeviceCapabilities& caps, DxvkDeviceQueueIndex queue) {
     DxvkDeviceQueue result = { };
     result.queueFamily = queue.family;
     result.queueIndex = queue.index;
+    result.properties = caps.getQueueProperties(queue.family);
 
     if (queue.family != VK_QUEUE_FAMILY_IGNORED)
       vkd->vkGetDeviceQueue(vkd->device(), queue.family, queue.index, &result.queueHandle);
@@ -244,9 +245,9 @@ namespace dxvk {
     Rc<vk::DeviceFn> vkd = new vk::DeviceFn(vk, true, device);
 
     DxvkDeviceQueueSet deviceQueues = { };
-    deviceQueues.graphics = getDeviceQueue(vkd, queueMapping.graphics);
-    deviceQueues.transfer = getDeviceQueue(vkd, queueMapping.transfer);
-    deviceQueues.sparse   = getDeviceQueue(vkd, queueMapping.sparse);
+    deviceQueues.graphics = getDeviceQueue(vkd, m_capabilities, queueMapping.graphics);
+    deviceQueues.transfer = getDeviceQueue(vkd, m_capabilities, queueMapping.transfer);
+    deviceQueues.sparse   = getDeviceQueue(vkd, m_capabilities, queueMapping.sparse);
 
     return new DxvkDevice(m_instance, this, vkd, m_capabilities.getFeatures(), deviceQueues, DxvkQueueCallback());
   }
@@ -278,9 +279,9 @@ namespace dxvk {
     Rc<vk::DeviceFn> vkd = new vk::DeviceFn(m_instance->vki(), false, args.device);
 
     DxvkDeviceQueueSet deviceQueues = { };
-    deviceQueues.graphics = getDeviceQueue(vkd, queueMapping.graphics);
-    deviceQueues.transfer = getDeviceQueue(vkd, queueMapping.transfer);
-    deviceQueues.sparse   = getDeviceQueue(vkd, queueMapping.sparse);
+    deviceQueues.graphics = getDeviceQueue(vkd, importCaps, queueMapping.graphics);
+    deviceQueues.transfer = getDeviceQueue(vkd, importCaps, queueMapping.transfer);
+    deviceQueues.sparse   = getDeviceQueue(vkd, importCaps, queueMapping.sparse);
 
     return new DxvkDevice(m_instance, this, vkd, importCaps.getFeatures(), deviceQueues, args.queueCallback);
   }

src/dxvk/dxvk_device.h
@@ -54,9 +54,11 @@ namespace dxvk {
    * queue family that it belongs to.
    */
   struct DxvkDeviceQueue {
-    VkQueue   queueHandle = VK_NULL_HANDLE;
-    uint32_t  queueFamily = 0;
-    uint32_t  queueIndex  = 0;
+    VkQueue       queueHandle = VK_NULL_HANDLE;
+    uint32_t      queueFamily = 0u;
+    uint32_t      queueIndex  = 0u;
+
+    DxvkDeviceQueueInfo properties = { };
   };
 
   /**

commit 8cf8759fd8e7119ac25f9a88cae98c39588de751

[dxvk] Fix OOB indexing by queue.family when queue type is absent
Happened on device that doesn't have sparse queue exposed.

Fixes: 8e5b7052 ("[dxvk] Expose queue ownership transfer properties in device")

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>

src/dxvk/dxvk_adapter.cpp
@@ -11,10 +11,11 @@ namespace dxvk {
     DxvkDeviceQueue result = { };
     result.queueFamily = queue.family;
     result.queueIndex = queue.index;
-    result.properties = caps.getQueueProperties(queue.family);
 
-    if (queue.family != VK_QUEUE_FAMILY_IGNORED)
+    if (queue.family != VK_QUEUE_FAMILY_IGNORED) {
+      result.properties = caps.getQueueProperties(queue.family);
       vkd->vkGetDeviceQueue(vkd->device(), queue.family, queue.index, &result.queueHandle);
+    }
 
     return result;
   }