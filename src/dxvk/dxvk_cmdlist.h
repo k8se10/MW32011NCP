doitsujin/dxvk

[dxvk] Use vkCmdPushConstants2 on the legacy path

src/dxvk/dxvk_cmdlist.h
@@ -956,13 +956,8 @@ namespace dxvk {
 
     void cmdPushConstants(
             DxvkCmdBuffer           cmdBuffer,
-            VkPipelineLayout        layout,
-            VkShaderStageFlags      stageFlags,
-            uint32_t                offset,
-            uint32_t                size,
-      const void*                   pValues) {
-      m_vkd->vkCmdPushConstants(getCmdBuffer(cmdBuffer),
-        layout, stageFlags, offset, size, pValues);
+      const VkPushConstantsInfo*    info) {
+      m_vkd->vkCmdPushConstants2(getCmdBuffer(cmdBuffer), info);
     }
 
 

k8se10/MW32011NCP

src/dxvk/dxvk_cmdlist.h
@@ -955,13 +955,8 @@ namespace dxvk {
 
     void cmdPushConstants(
             DxvkCmdBuffer           cmdBuffer,
-            VkPipelineLayout        layout,
-            VkShaderStageFlags      stageFlags,
-            uint32_t                offset,
-            uint32_t                size,
-      const void*                   pValues) {
-      m_vkd->vkCmdPushConstants(getCmdBuffer(cmdBuffer),
-        layout, stageFlags, offset, size, pValues);
+      const VkPushConstantsInfo*    info) {
+      m_vkd->vkCmdPushConstants2KHR(getCmdBuffer(cmdBuffer), info);
     }
 
 